c94a3bad605b33ce753e29b86ca8ad43561a7e30
remonandmaro <remonandmaro@gmail.com>
2025-08-28 15:45:21 +0300

    First

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using ERPSystem.Models;
+using ERPSystem.Tools;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace ERPSystem.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly MyDBContext db;
+
+        public BaseController(MyDBContext db)
+        {
+            this.db = db;
+            var Name = "Remon";
+            General ? G = db.Generals.FirstOrDefault();
+            if (G != null)
+            {
+                MainData.MainTitle = G.MainTitle;
+                MainData.Address = G.Address;
+                MainData.Mobile = G.Mobile;
+                MainData.StoreName = G.StoreName;
+                MainData.Mail = G.Mail;
+                MainData.DesignBy = G.DesignBy;
+            }
+        }
+    }
+}
---
+++ Migrations/20250826191252_First.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Generals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MainTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StoreName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DesignBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Generals", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Generals");
+        }
+    }
+}
